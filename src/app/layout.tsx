0373cc172e626f09f4bd587c890a4207886faf0e
Yuna Park <raina.y.park@gmail.com>
2023-08-13 18:23:00 +0900

    add: Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
+import "./globals.css";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-
         {children}
+        <Footer />
       </body>
     </html>
   );
