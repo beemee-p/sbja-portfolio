2360ac058bae629c5b8b02713030bc038132ce7c
Yuna Park <raina.y.park@gmail.com>
2023-08-14 23:51:30 +0900

    modify: Menu ui when open

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,13 +1,21 @@
 import { ReactElement } from "react";
 import styles from "@/app/styles/css/Menu.module.css";
 
+const menuList = ["포트폴리오", "블로그"];
 interface MenuProps {
   isMenu: boolean;
 }
+
 const Menu = (props: MenuProps): ReactElement => {
   return (
     <div className={`${styles.menu} ${props.isMenu ? styles.on : styles.off}`}>
-      Menu
+      <ul className={styles.menu_list}>
+        {menuList.map((menu, index) => (
+          <li key={index} className={styles.menu_item}>
+            {menu}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
