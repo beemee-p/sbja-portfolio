997885f205999fa8c946126da9ff35dc2497e17a
Yuna Park <raina.y.park@gmail.com>
2023-08-17 23:44:45 +0900

    add: List component w/ post data

---
+++ src/app/components/List.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react";
+import styles from "@/app/styles/css/List.module.css";
+import { getSortedPostsData } from "../../../lib/posts";
+import ListCard from "./ListCard";
+
+const List = (): ReactElement => {
+  const posts = getSortedPostsData();
+
+  return (
+    <div className={styles.list}>
+      <h1 className={styles.h1}>MUST READ</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <ListCard key={post.id} post={post} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import Banner from "./components/Banner";
+import List from "./components/List";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <h1>This is Test Title</h1>
-      <p>This is Test component</p>
-      <p>This is Test component</p>
-      <p>This is Test component</p>
-      <p>This is Test component</p>
-
-      <p>리스트 컴포넌트 들어갈 자리</p>
+      <List />
     </main>
   );
 }
