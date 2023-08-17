ff4b8e8aa4c4cee36bbc1565a1991c1b356f099e
Yuna Park <raina.y.park@gmail.com>
2023-08-17 23:44:45 +0900

    add: List component w/ post data

---
+++ src/app/components/ListCard.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react";
+import { Post } from "../types";
+import { getFormattedDate } from "../../../lib/getFormattedDate";
+import Link from "next/link";
+
+interface ListCardProps {
+  post: Post;
+}
+
+const ListCard = (props: ListCardProps): ReactElement => {
+  const { id, title, date } = props.post;
+  const createdAt = getFormattedDate(date);
+
+  return (
+    <li>
+      <Link href={`/post/${id}`}>{title}</Link>
+      <p>{createdAt}</p>
+    </li>
+  );
+};
+
+export default ListCard;
