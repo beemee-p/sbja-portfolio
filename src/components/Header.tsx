737a742025acc75493b0d4a12aac2b3b7bb98783
Yuna Park <raina.y.park@gmail.com>
2023-08-21 23:33:25 +0900

    fix: gnb bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,31 +9,29 @@ const Header = (): ReactElement => {
   const [isMenu, setIsMenu] = useState(false);
 
   return (
-    <>
-      <nav className={styles.header}>
-        <Link
-          className={`${styles.logo} ${isMenu ? styles.on : styles.off}`}
-          onClick={() => isMenu && setIsMenu(false)}
-          href="/"
-        >
-          SBJA 🐣
-        </Link>
+    <nav className={styles.header}>
+      <Link
+        className={`${styles.logo} ${isMenu ? styles.on : styles.off}`}
+        onClick={() => isMenu && setIsMenu(false)}
+        href="/"
+      >
+        SBJA 🐣
+      </Link>
 
-        <div
-          className={`${styles.menu_btn} ${isMenu ? styles.on : styles.off}`}
-          onClick={() => setIsMenu(!isMenu)}
-        >
-          {isMenu ? "CLOSE" : "MENU"}
+      <div
+        className={`${styles.menu_btn} ${isMenu ? styles.on : styles.off}`}
+        onClick={() => setIsMenu(!isMenu)}
+      >
+        {isMenu ? "CLOSE" : "MENU"}
+        <AiOutlineMenu
+          className={styles.menu_icon}
+          size={"24"}
+          color={`${isMenu ? "#ffffff" : "#000000"} `}
+        />
+      </div>
 
-          <AiOutlineMenu
-            className={styles.menu_icon}
-            size={"24"}
-            color={`${isMenu ? "#ffffff" : "#000000"} `}
-          />
-        </div>
-        <Menu isMenu={isMenu} close={() => setIsMenu(false)} />
-      </nav>
-    </>
+      {isMenu && <Menu isMenu={isMenu} close={() => setIsMenu(false)} />}
+    </nav>
   );
 };
 
