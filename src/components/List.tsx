5bf36bda2c832f419723eb409d56c3d1bb0eaa47
Yuna Park <raina.y.park@gmail.com>
2023-12-12 23:34:26 +0900

    add: GridList

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 import { getSortedPostsData } from "@/library/posts";
 import styles from "@/styles/css/List.module.css";
 import ListCard from "@/components/ListCard";
+import GridList from "./common/GridList";
 
 const List = (): ReactElement => {
   const posts = getSortedPostsData();
@@ -10,11 +11,16 @@ const List = (): ReactElement => {
     <div className={styles.list}>
       <h1 className={styles.h1}>MUST READ</h1>
 
-      <ul className={styles.ul}>
+      <GridList
+        itemPerLine={4}
+        rowSpacing={10}
+        columnSpacing={10}
+        direction={"row"}
+      >
         {posts.map((post) => (
           <ListCard key={post.id} post={post} />
         ))}
-      </ul>
+      </GridList>
     </div>
   );
 };
---
+++ src/components/common/GridList.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { ReactElement, ReactNode, useEffect } from "react";
+import styles from "@/styles/css/common/GridList.module.css";
+
+interface GridListProps {
+  children: ReactNode;
+  itemPerLine: number;
+  rowSpacing?: number;
+  columnSpacing?: number;
+  direction?: "column" | "row";
+}
+
+const GridList = (props: GridListProps): ReactElement => {
+  const rowMargin = props.rowSpacing || 0;
+  const columnMargin = props.columnSpacing || 0;
+  const restRowWidth = Math.floor(rowMargin / props.itemPerLine);
+
+  const width =
+    props.direction === "row"
+      ? `calc(100% / ${props.itemPerLine} - ${rowMargin}px + ${restRowWidth}px)`
+      : "100%";
+
+  useEffect(() => {
+    const children = document.querySelector(".grid")
+      ?.children as HTMLCollectionOf<HTMLElement>;
+
+    for (const childIndex in children) {
+      const child = children[+childIndex];
+
+      if (+childIndex >= 0) {
+        child.style.flexShrink = "0";
+        child.setAttribute("id", (+childIndex + 1).toString());
+        child.style.width = width;
+        child.style.marginRight = rowMargin.toString() + "px";
+        child.style.marginBottom = columnMargin.toString() + "px";
+
+        if (+child.id % props.itemPerLine === 0) {
+          child.style.marginRight = "0";
+        }
+      }
+    }
+  }, [
+    rowMargin,
+    columnMargin,
+    props.columnSpacing,
+    props.rowSpacing,
+    props.direction,
+    props.itemPerLine,
+    width,
+  ]);
+
+  return <div className={`grid ${styles.grid}`}>{props.children}</div>;
+};
+
+export default GridList;
