cae255e7f5c247174fba51e68c01f9bc4c48a6dc
Yuna Park <raina.y.park@gmail.com>
2023-12-19 00:07:04 +0900

    modify Heeader ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,24 @@
 "use client";
 import { ReactElement, useState } from "react";
+import { useParams } from "next/navigation";
 import { AiOutlineMenu } from "react-icons/ai";
+import { MdArrowBackIosNew } from "react-icons/md";
 import Link from "next/link";
 import Menu from "@/components/Menu";
 import styles from "@/styles/css/Header.module.css";
 
 const Header = (): ReactElement => {
+  const { portfolioID } = useParams();
   const [isMenu, setIsMenu] = useState(false);
 
   return (
     <header className={styles.header}>
+      {portfolioID && (
+        <Link className={styles.back_btn} href="/">
+          <MdArrowBackIosNew size={"20"} />
+        </Link>
+      )}
+
       <Link
         className={`${styles.logo} ${isMenu ? styles.on : styles.off}`}
         onClick={() => isMenu && setIsMenu(false)}
@@ -23,7 +32,6 @@ const Header = (): ReactElement => {
         onClick={() => setIsMenu(!isMenu)}
       >
         {isMenu ? "CLOSE" : "MENU"}
-
         <AiOutlineMenu
           className={styles.menu_icon}
           size={"24"}
