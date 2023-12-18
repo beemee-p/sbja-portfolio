907fa08f89297fc10e41f9ef9e6079f81691f3a6
Yuna Park <raina.y.park@gmail.com>
2023-12-19 00:07:04 +0900

    modify Heeader ui

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,43 +4,69 @@ import Link from "next/link";
 import styles from "@/styles/css/Menu.module.css";
 import { useRouter } from "next/router";
 
-const menuList = [
-  { title: "PORTFOLIO", url: `/portfolio` },
-  {
-    title: (
-      <>
-        BLOG <TbExternalLink />
-      </>
-    ),
-    url: `https://devbirdfeet.tistory.com`,
-  },
-  {
-    title: (
-      <>
-        RESUME <TbExternalLink />
-      </>
-    ),
-    url: `https://beemeep.notion.site/yuna-park-3043560342374634a8c051c4b8bbfe4c?pvs=4`,
-  },
-  {
-    title: (
-      <>
-        GITHUB <TbExternalLink />
-      </>
-    ),
-    url: `https://github.com/beemee-p`,
-  },
-];
 interface MenuProps {
   isMenu: boolean;
   close: () => void;
 }
 
 const Menu = (props: MenuProps): ReactElement => {
+  const menuList = [
+    {
+      title: "PORTFOLIO",
+      linkProps: {
+        onClick: handleMoveScroll,
+        href: "/",
+      },
+    },
+    {
+      title: (
+        <>
+          BLOG <TbExternalLink />
+        </>
+      ),
+      linkProps: {
+        href: "https://devbirdfeet.tistory.com",
+        target: "_blank",
+      },
+    },
+    {
+      title: (
+        <>
+          RESUME <TbExternalLink />
+        </>
+      ),
+      linkProps: {
+        href: "https://beemeep.notion.site/yuna-park-3043560342374634a8c051c4b8bbfe4c?pvs=4",
+        target: "_blank",
+      },
+    },
+    {
+      title: (
+        <>
+          GITHUB <TbExternalLink />
+        </>
+      ),
+      linkProps: {
+        href: "https://github.com/beemee-p",
+        target: "_blank",
+      },
+    },
+  ];
+
   function handleClose(e: React.MouseEvent<HTMLElement>) {
     props.close();
   }
 
+  function handleMoveScroll(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    e.preventDefault();
+    const list = document.querySelector(".list-wrap") as HTMLDivElement | null;
+    list?.scrollIntoView({ behavior: "smooth" });
+
+    const offset: number | undefined = (list?.offsetTop as number) - 93;
+    window.scrollTo({ top: offset, behavior: "smooth" });
+  }
   return (
     <div
       className={`${styles.menu} ${props.isMenu ? styles.on : styles.off}`}
@@ -48,12 +74,7 @@ const Menu = (props: MenuProps): ReactElement => {
     >
       <div className={styles.menu_list}>
         {menuList.map((menu, index) => (
-          <Link
-            key={index}
-            className={styles.menu_item}
-            href={menu.url}
-            target="_blank"
-          >
+          <Link key={index} className={styles.menu_item} {...menu.linkProps}>
             {menu.title}
           </Link>
         ))}
