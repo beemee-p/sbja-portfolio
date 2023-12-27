9965299a23deb3fa90b002c4256f070a49d1bc19
Yuna Park <raina.y.park@gmail.com>
2023-12-27 17:42:51 +0900

    add: PortfolioInfo panel to modal

---
+++ src/components/portfolio/PortfolioInfo.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { ReactElement, useState } from "react";
+import styles from "@/styles/css/portfolio/PortfolioInfo.module.css";
+import Button from "../common/Button";
+import { IoCloseOutline } from "react-icons/io5";
+import { getPostData } from "@/library/posts";
+
+interface PortfolioInfoProps {
+  contents: string;
+}
+
+const PortfolioInfo = (props: PortfolioInfoProps): ReactElement => {
+  const [showInfo, setShowInfo] = useState(true);
+  // TODO: 등장 퇴장시 애니메이션 추가
+
+  return showInfo ? (
+    <div className={styles.info_wrap}>
+      <section
+        className={styles.info_content}
+        dangerouslySetInnerHTML={{ __html: props.contents }}
+      />
+      <Button
+        className={styles.info_close_btn}
+        onClick={() => setShowInfo(false)}
+      >
+        <IoCloseOutline size={"24"} color="#7C8484" />
+      </Button>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default PortfolioInfo;
