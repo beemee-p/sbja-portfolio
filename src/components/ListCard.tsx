0489a77503cc56a5042e328115bf65a1c7cfd1b3
Yuna Park <raina.y.park@gmail.com>
2024-01-05 00:19:31 +0900

    add: Badge component

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -5,31 +5,45 @@ import { Portfolio } from "@/app/types";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/css/ListCard.module.css";
+import Badge from "./common/Badge";
 
 interface ListCardProps {
-  post: Portfolio;
+  portfolio: Portfolio;
 }
 
 const ListCard = (props: ListCardProps): ReactElement => {
-  const { id, title, date, thumbnail } = props.post;
-  const createdAt = getFormattedDate(date);
+  const createdAt = getFormattedDate(props.portfolio.date);
 
   return (
     <li className={styles.list_card}>
-      <Link className={styles.list_card_link} href={`/portfolio/${id}`}>
-        {thumbnail && (
-          <Image
-            className={styles.list_card_img}
-            alt="thumbnail"
-            src={thumbnail}
-            style={{ width: "100%", height: "100%" }}
-            fill={true}
-          />
+      <Link
+        className={styles.list_card_link}
+        href={`/portfolio/${props.portfolio.id}`}
+      >
+        {props.portfolio.thumbnail && (
+          <div className={styles.list_card_img_wrap}>
+            <div className={styles.list_card_img}>
+              <Image
+                // className={styles.list_card_img}
+                alt="thumbnail"
+                src={props.portfolio.thumbnail}
+                style={{ width: "100%", height: "100%" }}
+                fill={true}
+              />
+              <div className={styles.deem}></div>
+            </div>
+          </div>
         )}
 
-        <div className={styles.list_card_text}>
-          <h3 className={styles.list_card_title}>{title}</h3>
-          <p className={styles.list_card_createdAt}>{createdAt}</p>
+        <div className={styles.list_card_info}>
+          <h3 className={styles.list_card_title}>{props.portfolio.title}</h3>
+          <div className={styles.list_card_badge}>
+            {props.portfolio.skills?.map((skill, index) => (
+              <Badge key={index} className={`${skill} badge`}>
+                {skill}
+              </Badge>
+            ))}
+          </div>
         </div>
       </Link>
     </li>
