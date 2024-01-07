8c93b1643ba5b8e1c52b1b610271dd3bf0f9ad31
Yuna Park <raina.y.park@gmail.com>
2024-01-07 18:01:45 +0900

    udpate: to disable outer scroll for Modal

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -7,6 +7,7 @@ import React, {
   useRef,
 } from "react";
 import styles from "@/styles/css/common/Modal.module.css";
+import { handleDisableBodyScroll } from "@/utils/scroll";
 import { useRouter } from "next/navigation";
 import { AiOutlineClose } from "react-icons/ai";
 import Button from "./Button";
@@ -54,57 +55,11 @@ const Modal = ({
   }
 
   useEffect(() => {
-    const root = document.querySelector<HTMLDivElement>("#root");
-    const app = document.querySelector<HTMLDivElement>("#app");
-
-    if (root?.classList.contains(DISABLED_SCROLL_CLASSNAME)) {
+    if (!disableBodyScroll) {
       return;
     }
-    root?.classList.add(DISABLED_SCROLL_CLASSNAME);
 
-    const scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    const hasScroll =
-      document.documentElement.scrollHeight >
-      document.documentElement.clientHeight;
-    document.documentElement.style.overflow = "hidden";
-    document.body.style.overflowY = hasScroll ? "scroll" : "hidden";
-    document.body.style.overflowX = "hidden";
-    document.body.style.position = "absolute";
-    document.body.style.width = "100%";
-    document.body.style.height = "100%";
-
-    if (app) {
-      app.style.minHeight = "100%";
-      app.style.height = "fit-content";
-    }
-
-    if (root) {
-      root.style.overflow = "hidden";
-      root.scrollTop = scrollTop;
-    }
-
-    return () => {
-      document.documentElement.style.overflow = "";
-      document.body.style.overflowY = "";
-      document.body.style.overflowX = "";
-      document.body.style.position = "";
-      document.body.style.width = "";
-      document.body.style.height = "";
-
-      if (app) {
-        app.style.minHeight = "";
-        app.style.height = "";
-      }
-
-      if (root) {
-        root.style.overflow = "";
-        root.scrollTop = 0;
-      }
-
-      document.documentElement.scrollTop = scrollTop;
-      root?.classList.remove(DISABLED_SCROLL_CLASSNAME);
-    };
+    handleDisableBodyScroll();
   }, [disableBodyScroll]);
 
   return (
