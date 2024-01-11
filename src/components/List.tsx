18a6a7e179fe4adf207ce26f0256bbbdc3e9c75f
Yuna Park <raina.y.park@gmail.com>
2024-01-11 23:48:10 +0900

    add: DeviceContext n apply to Grid

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,26 +1,28 @@
-import { ReactElement } from "react";
-import { getSortedPostsData } from "@/library/posts";
-import styles from "@/styles/css/List.module.css";
-import ListCard from "@/components/ListCard";
+"use client";
+import { Portfolio } from "@/app/types";
+import React, { ReactElement } from "react";
 import Grid from "./common/Grid";
+import { useDeviceContext } from "./DeviceContext";
+import ListCard from "./ListCard";
 
-const List = (): ReactElement => {
-  const portfolios = getSortedPostsData();
+interface ListProps {
+  portfolios: Portfolio[];
+}
+
+const List = (props: ListProps): ReactElement => {
+  const { isMobile, isTablet } = useDeviceContext();
 
   return (
-    <div className={`list-wrap ${styles.list_wrap}`}>
-      <h1 className={styles.h1}>PORTFOLIOS</h1>
-      <Grid
-        itemPerLine={4}
-        rowSpacing={20}
-        columnSpacing={40}
-        direction={"row"}
-      >
-        {portfolios.map((portfolio) => (
-          <ListCard key={portfolio.id} portfolio={portfolio} />
-        ))}
-      </Grid>
-    </div>
+    <Grid
+      itemPerLine={isMobile ? 1 : isTablet ? 3 : 4}
+      rowSpacing={20}
+      columnSpacing={40}
+      direction={"row"}
+    >
+      {props.portfolios?.map((portfolio) => (
+        <ListCard key={portfolio.id} portfolio={portfolio} />
+      ))}
+    </Grid>
   );
 };
 
---
+++ src/components/ListWrap.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react";
+import { getSortedPostsData } from "@/library/posts";
+import styles from "@/styles/css/List.module.css";
+import ListCard from "@/components/ListCard";
+import Grid from "./common/Grid";
+import { useDeviceContext } from "./DeviceContext";
+import List from "./List";
+
+const ListWrap = (): ReactElement => {
+  const portfolios = getSortedPostsData();
+
+  return (
+    <div className={`list-wrap ${styles.list_wrap}`}>
+      <h1 className={styles.h1}>PORTFOLIOS</h1>
+      <List portfolios={portfolios} />
+    </div>
+  );
+};
+
+export default ListWrap;
